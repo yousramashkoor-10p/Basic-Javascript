9b1d2f3127562d950667fa1b3f9a53540c324749
yousra.mashkoor <yousra.mashkoor@tenpearls.com>
2021-01-14 18:17:34 +0500

    finalizing basic JS

---
+++ count-backwards-with-a-for-loop.js
@@ -0,0 +1,8 @@
+// Setup
+var myArray = [];
+
+// Only change code below this line
+
+for (var i = 9; i > 0; i -= 2) {
+  myArray.push(i);
+}
---
+++ iterate-with-javascript-while-loops.js
@@ -0,0 +1,9 @@
+// Setup
+var myArray = [];
+
+// Only change code below this line
+var i = 0;
+while(i < 6) {
+  myArray.unshift(i);
+  i++;
+}
